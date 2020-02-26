5bb3f02197682fe56c295f1a7bd72307de88cd24
Harijs Grinbergs <1474290+TheExDeus@users.noreply.github.com>
2020-02-26 18:16:27 +0000

    Enabled motononic timeout on POSIX
    -Previously timeout triggered if system clock changed. That was probably not the idea. Now timeout is monotonic and triggers at correct time even if clock changed.

--- sdk/sdk/src/hal/event.h
+++ sdk/sdk/src/hal/event.h
@@ -8,26 +8,26 @@
  *
  */
 /*
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, 
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
@@ -38,14 +38,14 @@ namespace rp{ namespace hal{
 class Event
 {
 public:
-    
+
     enum
     {
         EVENT_OK = 1,
         EVENT_TIMEOUT = -1,
         EVENT_FAILED = 0,
     };
-    
+
     Event(bool isAutoReset = true, bool isSignal = false)
 #ifdef _WIN32
         : _event(NULL)
@@ -55,10 +55,13 @@ public:
 #endif
     {
 #ifdef _WIN32
-        _event = CreateEvent(NULL, isAutoReset?FALSE:TRUE, isSignal?TRUE:FALSE, NULL); 
+        _event = CreateEvent(NULL, isAutoReset?FALSE:TRUE, isSignal?TRUE:FALSE, NULL);
 #else
         pthread_mutex_init(&_cond_locker, NULL);
         pthread_cond_init(&_cond_var, NULL);
+        pthread_condattr_init(&_attr);
+        pthread_condattr_setclock(&_attr, CLOCK_MONOTONIC);
+        pthread_cond_init(&_cond_var, &_attr);
 #endif
     }
 
@@ -74,7 +77,7 @@ public:
             SetEvent(_event);
 #else
             pthread_mutex_lock(&_cond_locker);
-               
+
             if ( _is_signalled == false )
             {
                 _is_signalled = true;
@@ -94,7 +97,7 @@ public:
 #endif
         }
     }
-    
+
     unsigned long wait( unsigned long timeout = 0xFFFFFFFF )
     {
 #ifdef _WIN32
@@ -114,7 +117,7 @@ public:
 
         if ( !_is_signalled )
         {
-            
+
                 if (timeout == 0xFFFFFFFF){
                     pthread_cond_wait(&_cond_var,&_cond_locker);
                 }else
@@ -125,7 +128,7 @@ public:
 
                     wait_time.tv_sec = timeout/1000 + now.tv_sec;
                     wait_time.tv_nsec = (timeout%1000)*1000000ULL + now.tv_usec*1000;
-                
+
                     if (wait_time.tv_nsec >= 1000000000)
                     {
                        ++wait_time.tv_sec;
@@ -145,10 +148,10 @@ public:
                         ans = EVENT_FAILED;
                         goto _final;
                     }
-       
+
             }
         }
-          
+
         assert(_is_signalled);
 
         if ( _isAutoReset )
@@ -160,7 +163,7 @@ _final:
 
         return ans;
 #endif
-        
+
     }
 protected:
 
@@ -171,12 +174,14 @@ protected:
 #else
         pthread_mutex_destroy(&_cond_locker);
         pthread_cond_destroy(&_cond_var);
+        pthread_condattr_destroy(&_attr);
 #endif
     }
 
 #ifdef _WIN32
         HANDLE _event;
 #else
+        pthread_condattr_t     _attr;
         pthread_cond_t         _cond_var;
         pthread_mutex_t        _cond_locker;
         bool                   _is_signalled;
